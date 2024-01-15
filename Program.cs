d1cf7c0ff770dd7ca1a3996fc71299af4901b836
Aleksei Obedkov <samarazap@gmail.com>
2024-01-15 22:31:56 +0400

    Добавлен код программы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] LessThan(string[] input, int n) {
+    string[] output = new string[CountArray(input, n)];
+    for(int i = 0, j = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            output[j] = input[i];
+            j++;
+        }
+    }
+    return output;
+}
+
+int CountArray(string[] input, int n) {
+    int count = 0;
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+    return count;
+}
+
+string[] MyArray() {
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+string[] arrayIn = MyArray();
+string[] arrayOut = LessThan(arrayIn, 3);
+Console.WriteLine($"[{string.Join(", ", arrayIn)}] -> [{string.Join(", ", arrayOut)}]");
